134c89da4a3d5d46acd1ca4c352e833e197ffe22
pachristopher <37901169+pachristopher@users.noreply.github.com>
2022-05-03 17:07:13 +0100

    Update m_i.R
    Fixed bug by changing rho0 to rho[1,] in 2 places.

--- R/m_i.R
+++ R/m_i.R
@@ -13,8 +13,8 @@
 m_i <- function(rho, m0, beta, age_vec){
   #check that length of parameter vectors match
   stopifnot("length of parameter vectors representing sub-populations need to be equal"= length(rho[1,])==length(m0))
-  m <- matrix(nrow = length(age_vec), ncol = length(rho0))
-  for(i in 1:length(rho0)){
+  m <- matrix(nrow = length(age_vec), ncol = length(rho[1,]))
+  for(i in 1:length(rho[1,])){
     m[,i] <- cbind((rho[,i]*m0[i]*exp(beta[i]*age_vec))/(1+0.5*m0[i]*exp(beta[i]*age_vec)))
   }
   numerator <- rowSums(cbind(m))
